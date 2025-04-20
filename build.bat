4793727ff90c9d5361ee51f9daa6536ec38e160c
SorbanElod <sorbanelod03@gmail.com>
2025-04-20 20:04:01 +0300

    copy executables to target location

--- build.bat
+++ build.bat
@@ -5,7 +5,11 @@ protoc --go_out=proto --go-grpc_out=proto --go_opt=paths=source_relative --go-gr
 echo Building gRPC server...
 go build -o gopass-grpc.exe cmd\server\main.go
 
+echo Copying executable to target directory...
+copy gopass-grpc.exe ..\DecentPass\DecentPass\Platforms\Windows
+
 echo Building CLI...
 go build
+copy gopass.exe ..\DecentPass\DecentPass\Platforms\Windows
 
 echo Done.
