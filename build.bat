4b7f4ae00c6052243a884e03418fe26c796b667a
SorbanElod <sorbanelod03@gmail.com>
2025-04-29 17:00:18 +0300

    delete old executables before build

--- build.bat
+++ build.bat
@@ -6,10 +6,12 @@ echo Building gRPC server...
 go build -o gopass-grpc.exe cmd\server\main.go
 
 echo Copying executable to target directory...
+del ..\DecentPass\DecentPass\Platforms\Windows\gopass-grpc.exe
 copy gopass-grpc.exe ..\DecentPass\DecentPass\Platforms\Windows
 
 echo Building CLI...
 go build
+del ..\DecentPass\DecentPass\Platforms\Windows\gopass.exe
 copy gopass.exe ..\DecentPass\DecentPass\Platforms\Windows
 
 echo Done.
